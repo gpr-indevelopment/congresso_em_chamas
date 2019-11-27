cb43e936fd8c6111dd6ec90f9da2b6d8dd0ef2a7
gpr-indevelopment <49736266+gpr-indevelopment@users.noreply.github.com>
2019-11-26 21:50:29 -0300

    Created spring boot backend
    I noticed that maybe applying react to a spring boot backend will be easier for me to learn javascript. I will replicate everything I have done in node to a spring application.

---
+++ Spring Backend/CongressoEmChamas/src/main/java/com/devindev/congressoemchamas/CongressoEmChamasApplication.java
@@ -0,0 +1,13 @@
+package com.devindev.congressoemchamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressoEmChamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongressoEmChamasApplication.class, args);
+	}
+
+}
---
+++ Spring Backend/CongressoEmChamas/src/test/java/com/devindev/congressoemchamas/CongressoEmChamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devindev.congressoemchamas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressoEmChamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
