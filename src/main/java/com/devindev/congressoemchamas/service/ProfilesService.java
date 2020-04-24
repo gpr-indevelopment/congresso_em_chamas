071bd48f9ad04b0c61383918b728c9059d92ab0c
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-04-23 23:13:07 -0300

    Fixed a bug where the loading spinner for propositions was out of place

--- src/main/java/com/devindev/congressoemchamas/service/ProfilesService.java
+++ src/main/java/com/devindev/congressoemchamas/service/ProfilesService.java
@@ -5,7 +5,6 @@ import com.devindev.congressoemchamas.externalapi.camara.CamaraAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ProfilesService {
