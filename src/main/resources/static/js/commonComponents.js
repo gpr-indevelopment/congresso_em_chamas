be008decf07a9c5926027dd335157eb75858441a
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-05-11 22:13:54 -0300

    Added profile picture adjusted for mobile

--- src/main/resources/static/js/commonComponents.js
+++ src/main/resources/static/js/commonComponents.js
@@ -12,3 +12,10 @@ function clearLoadingState(){
         globalOverlay.style.display = "none";
     })
 }
+
+function renderProfile(element, politician) {
+    element.innerHTML = "";
+    element.insertAdjacentHTML("beforeend", `<img src="${politician.picture}" class="rounded-circle profile-picture mb-2">`);
+    element.insertAdjacentHTML("beforeend", `<div class="heading text-center mb-0">${politician.name}</div>`)
+    element.insertAdjacentHTML("beforeend", `<div class="common-text text-center mb-0">${politician.party}</div>`)
+}
