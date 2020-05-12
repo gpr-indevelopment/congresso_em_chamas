318d9b9fd40f86bd3e5ad42a42eb4e9df22f706d
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-05-11 22:57:10 -0300

    Added politician profile rendering on all pages

--- src/main/resources/static/js/commonComponents.js
+++ src/main/resources/static/js/commonComponents.js
@@ -15,7 +15,11 @@ function clearLoadingState(){
 
 function renderProfile(element, politician) {
     element.innerHTML = "";
-    element.insertAdjacentHTML("beforeend", `<img src="${politician.picture}" class="rounded-circle profile-picture mb-2">`);
-    element.insertAdjacentHTML("beforeend", `<div class="heading text-center mb-0">${politician.name}</div>`)
-    element.insertAdjacentHTML("beforeend", `<div class="common-text text-center mb-0">${politician.party}</div>`)
+    element.insertAdjacentHTML("beforeend", `<div class="politician-profile-picture-container rounded-circle">
+                                                <img src="${politician.picture}" class="w-100">
+                                            </div>`);
+    element.insertAdjacentHTML("beforeend", `<div class="ml-2 d-flex flex-column justify-content-center">
+                                                <div class="heading text-center mb-1">${politician.name}</div>
+                                                <div class="common-text text-center">${politician.party}</div>
+                                            </div>`)
 }
