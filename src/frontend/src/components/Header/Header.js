ecc27d33599f4ca23878c3cad646d7f8ab4b807a
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-19 14:24:49 -0300

    Added a link on top of the header logo to the landing page

--- src/frontend/src/components/Header/Header.js
+++ src/frontend/src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Header.module.css";
 import * as CongressoComponents from "../index";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <div className={styles.container}>
-      <div>
+      <Link to="/">
         <CongressoComponents.HeaderLogo />
-      </div>
+      </Link>
       <div>{props.children}</div>
     </div>
   );
