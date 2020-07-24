85b97dd4496ec1787e9e2c5d4742489c5dab71ae
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-24 20:54:24 -0300

    Added header menu as a Drawer

--- src/frontend/src/components/Header/Header.js
+++ src/frontend/src/components/Header/Header.js
@@ -1,23 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Profile from "./Profile";
 import styles from "./Header.module.css";
+import { Affix, Button } from "antd";
+import HeaderDrawer from "./HeaderDrawer";
+import { MenuOutlined } from "@ant-design/icons";
 import HeaderLogo from "./HeaderLogo";
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
   let { handleProfileLoading, politicianId, loading } = props;
   useEffect(() => {
     politicianId && handleProfileLoading(politicianId);
   }, [handleProfileLoading, politicianId]);
   return (
     <div className={styles.container}>
-      <Link to="/">
-        <HeaderLogo />
-      </Link>
+      {politicianId ? (
+        <Affix offsetTop={25}>
+          <Button
+            type="primary"
+            icon={<MenuOutlined />}
+            onClick={() => setDrawerVisible(!drawerVisible)}
+          />
+        </Affix>
+      ) : (
+        <Link to="/">
+          <HeaderLogo />
+        </Link>
+      )}
+
       <div>{props.children}</div>
       {politicianId && (
         <Profile profile={props.profile} loading={politicianId && loading} />
       )}
+      <HeaderDrawer
+        visible={drawerVisible}
+        onClose={() => setDrawerVisible(false)}
+      />
     </div>
   );
 }
---
+++ src/frontend/src/components/Header/HeaderDrawer.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Drawer, Menu, Row, Col } from "antd";
+import logo from "../../assets/perfil-nobackground.png";
+import styles from "./HeaderDrawer.module.css";
+import { Link } from "react-router-dom";
+import { faFileAlt } from "@fortawesome/free-regular-svg-icons";
+import { faNewspaper, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PoliticianSearch from "../PoliticianSearch";
+
+function HeaderDrawer(props) {
+  let pathname = window.location.pathname;
+  const politicianId = new URLSearchParams(window.location.search).get(
+    "politician"
+  );
+  return (
+    <Drawer
+      visible={props.visible}
+      onClose={props.onClose}
+      placement="left"
+      title={
+        <Link to="/">
+          <div className={styles.title}>
+            <img src={logo} height={70} alt="No content available" />
+          </div>
+        </Link>
+      }
+    >
+      <Menu>
+        <Menu.Item disabled={pathname === "/search"}>
+          <PoliticianSearch
+            handleSearchSubmit={(input) =>
+              window.open(`/search?politicianName=${input}`)
+            }
+          />
+        </Menu.Item>
+        <Menu.Item disabled={pathname === "/expenses"}>
+          <Link to={`/expenses?politician=${politicianId}`}>
+            <Row>
+              <Col span={6} className={styles.icon}>
+                <FontAwesomeIcon icon={faDollarSign} size="lg" />
+              </Col>
+              <Col span={18}>Despesas</Col>
+            </Row>
+          </Link>
+        </Menu.Item>
+        <Menu.Item disabled={pathname === "/news"}>
+          <Link to={`/news?politician=${politicianId}`}>
+            <Row>
+              <Col span={6} className={styles.icon}>
+                <FontAwesomeIcon icon={faNewspaper} size="lg" />
+              </Col>
+              <Col span={18}>Notícias</Col>
+            </Row>
+          </Link>
+        </Menu.Item>
+        <Menu.Item disabled={pathname === "/propositions"}>
+          <Link to={`/propositions?politician=${politicianId}`}>
+            <Row>
+              <Col span={6} className={styles.icon}>
+                <FontAwesomeIcon icon={faFileAlt} size="lg" />
+              </Col>
+              <Col span={18}>Proposições</Col>
+            </Row>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Drawer>
+  );
+}
+
+export default HeaderDrawer;
