d2b2756e048e4ffe0ef7e990262dbe1aac853259
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-24 21:16:55 -0300

    Added new frontend build

---
+++ src/main/resources/static/precache-manifest.0cef1b667c6e29f1c5657d6e3159ad50.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728ac7aa3e15465c2c2fc6f01ff0fa0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c08dc6f37d837437ba",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "0b6cc3491e3a2fa17ac3",
+    "url": "/static/css/main.a32424b0.chunk.css"
+  },
+  {
+    "revision": "d9c08dc6f37d837437ba",
+    "url": "/static/js/2.c18198cd.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.c18198cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6cc3491e3a2fa17ac3",
+    "url": "/static/js/main.43377b16.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "213b75c72f7a841bdc20fa3726964765",
+    "url": "/static/media/github-icon.213b75c7.png"
+  },
+  {
+    "revision": "66ba5b2afd568e229a94a779c75696ab",
+    "url": "/static/media/perfil-nobackground.66ba5b2a.png"
+  },
+  {
+    "revision": "611380c84a347f1ced038a1a8ece6197",
+    "url": "/static/media/poster.611380c8.png"
+  },
+  {
+    "revision": "c98cd51dbc59f910fca497326943921c",
+    "url": "/static/media/protests_144.c98cd51d.mp4"
+  },
+  {
+    "revision": "2b216380c34369a5fe4322a1db3aab47",
+    "url": "/static/media/protests_240.2b216380.mp4"
+  }
+]);
