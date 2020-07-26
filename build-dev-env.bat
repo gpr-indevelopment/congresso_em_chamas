1a791cf6eb3f0e43d045032f6dabdfe09e6ebc27
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-26 17:54:27 -0300

    Created a dev environment startup script

---
+++ build-dev-env.bat
@@ -0,0 +1,6 @@
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+timeout 60
+docker-compose -f docker-compose-dev.yml up -d
+start "" code frontend
+start "" idea64 backend/pom.xml
+exit
