4b5fa4d7039118b7fce370a6fd99e3d5a0d94055
gpr-indevelopment <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-29 14:59:14 -0300

    Finished access log filter impl. Now all access is persisted.

--- backend/src/main/java/com/devindev/congressoemchamas/AccessLogFilter.java
+++ backend/src/main/java/com/devindev/congressoemchamas/AccessLogFilter.java
@@ -1,6 +1,8 @@
 package com.devindev.congressoemchamas;
 
 import com.devindev.congressoemchamas.data.accesslog.AccessLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
@@ -8,7 +10,6 @@ import org.springframework.util.StopWatch;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Objects;
@@ -19,19 +20,26 @@ public class AccessLogFilter implements Filter {
     @Autowired
     private MainRepository mainRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessLogFilter.class);
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         filterChain.doFilter(servletRequest, servletResponse);
         stopWatch.stop();
-        logAccess(servletRequest, servletResponse, stopWatch.getLastTaskTimeMillis());
+        try {
+            logAccess(servletRequest, stopWatch.getLastTaskTimeMillis());
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            LOGGER.error(e.getStackTrace()[0].toString());
+            LOGGER.error("An error was found while trying to save an access log. Skipping...");
+        }
     }
 
-    private void logAccess(ServletRequest servletRequest, ServletResponse servletResponse, Long processingTimeMillis) {
+    private void logAccess(ServletRequest servletRequest, Long processingTimeMillis) {
         AccessLog accessLog = new AccessLog();
         HttpServletRequest req = (HttpServletRequest) servletRequest;
-        HttpServletResponse res = (HttpServletResponse) servletResponse;
         accessLog.setMethod(HttpMethod.valueOf(req.getMethod()));
         accessLog.setTimestamp(new Timestamp(System.currentTimeMillis()));
         accessLog.setEndpoint(buildEndpointString(req));
