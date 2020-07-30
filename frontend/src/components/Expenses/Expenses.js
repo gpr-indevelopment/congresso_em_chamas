97973564114743544e442d2c1e91b9665fe999f4
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-29 23:26:47 -0300

    CEC-2
    Added and positioned the radio buttons on top of the expenses chart.

--- frontend/src/components/Expenses/Expenses.js
+++ frontend/src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import { Header, MainContent, Footer } from "../";
 import ExpensesGraph from "./ExpensesGraph";
 import ExpensesDetailsSection from "./ExpensesDetailsSection";
 import styles from "./Expenses.module.css";
-import { Spin } from "antd";
+import { Spin, Radio, Space } from "antd";
 import EmptyData from "../EmptyData";
 
 function Expenses(props) {
@@ -22,18 +22,28 @@ function Expenses(props) {
         {props.expenseData.monthlyExpenses &&
         props.expenseData.monthlyExpenses.length > 0 ? (
           <div className={styles.container}>
-            <div className={styles.chart}>
-              <ExpensesGraph
-                data={props.expenseData}
-                onDataClick={(event, array) =>
-                  array.length > 0 && props.handleDetailsClick(array[0]._index)
-                }
-              />
-            </div>
+            <Space direction="vertical" className={styles.left}>
+              <div className={styles.radio}>
+                <Radio.Group defaultValue="1" buttonStyle="solid">
+                  <Radio.Button value="1">Esse ano</Radio.Button>
+                  <Radio.Button value="2">Últimos 6 meses</Radio.Button>
+                  <Radio.Button value="3">Legislatura</Radio.Button>
+                </Radio.Group>
+              </div>
+              <div className={styles.chart}>
+                <ExpensesGraph
+                  data={props.expenseData}
+                  onDataClick={(event, array) =>
+                    array.length > 0 &&
+                    props.handleDetailsClick(array[0]._index)
+                  }
+                />
+              </div>
+            </Space>
             <ExpensesDetailsSection data={props.detailsData} />
           </div>
         ) : (
-          <EmptyData/>
+          <EmptyData />
         )}
       </MainContent>
       <Footer />
