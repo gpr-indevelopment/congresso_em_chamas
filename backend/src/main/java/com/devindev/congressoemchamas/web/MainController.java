f08b0b78fc7b49d195c464ff290100c6512f9e3c
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-30 00:00:57 -0300

    CEC-2
    Removed unused endpoints from the main controller

--- backend/src/main/java/com/devindev/congressoemchamas/web/MainController.java
+++ backend/src/main/java/com/devindev/congressoemchamas/web/MainController.java
@@ -43,96 +43,14 @@ public class MainController {
     }
 
     @CrossOrigin(origins = "*")
-    @RequestMapping(path = "/politicians/{politicianId}/monthlyexpenses/{legislatureId}", method = RequestMethod.GET)
-    public List<MonthlyExpense> getMonthlyExpensesByPoliticianId(@PathVariable Long politicianId, @PathVariable Long legislatureId) {
-        return mainService.findByLegislature(politicianId, legislatureId);
-    }
-
-    @CrossOrigin(origins = "*")
     @RequestMapping(path = "/politicians/{politicianId}/monthlyexpenses", method = RequestMethod.GET)
-    public List<MonthlyExpense> getMonthlyExpensesByPoliticianId(@PathVariable Long politicianId, @RequestParam(required = false) List<Integer> months, @RequestParam(required = false) List<Integer> years) {
+    public List<MonthlyExpense> getMonthlyExpenses(@PathVariable Long politicianId, @RequestParam(required = false) List<Integer> months, @RequestParam(required = false) List<Integer> years) {
         return mainService.findMonthlyExpensesByPoliticianId(politicianId, months, years);
-    }
-
-    @CrossOrigin(origins = "*")
-    @RequestMapping(path = "/legislature", method = RequestMethod.GET)
-    public Legislature getCurrentLegislature() {
-        return mainService.getCurrentLegislature();
     }
 
     @CrossOrigin(origins = "*")
     @RequestMapping(path = "/profiles", method = RequestMethod.GET)
     public List<Profile> getProfilesByName(@RequestParam String name) {
         return mainService.findProfilesByName(name);
-    }
-
-    @CrossOrigin(origins = "*")
-    @RequestMapping(method = RequestMethod.POST, path = "/document/upload")
-    public Document upload(@RequestParam("file") MultipartFile file) {
-        try {
-            Thread.sleep(10000l);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Document doc = new Document();
-        Random random = new Random();
-        doc.setNumber(Integer.valueOf(random.nextInt(100000)).longValue());
-        doc.setPart("samplepart");
-        doc.setType("sampletype");
-        doc.setDesignOffice("WEG");
-        doc.setVersion(01);
-        return doc;
-    }
-
-    private class Document {
-        private Long number;
-
-        private String type;
-
-        private String part;
-
-        private Integer version;
-
-        private String designOffice;
-
-        public Long getNumber() {
-            return number;
-        }
-
-        public void setNumber(Long number) {
-            this.number = number;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getPart() {
-            return part;
-        }
-
-        public void setPart(String part) {
-            this.part = part;
-        }
-
-        public Integer getVersion() {
-            return version;
-        }
-
-        public void setVersion(Integer version) {
-            this.version = version;
-        }
-
-        public String getDesignOffice() {
-            return designOffice;
-        }
-
-        public void setDesignOffice(String designOffice) {
-            this.designOffice = designOffice;
-        }
     }
 }
