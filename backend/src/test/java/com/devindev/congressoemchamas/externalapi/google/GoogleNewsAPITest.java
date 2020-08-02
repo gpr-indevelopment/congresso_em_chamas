227bb40e7e8fe388ef0415558d8133f0b8ca0486
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-08-02 18:36:39 -0300

    CEC-4
    Finished tests for GoogleNewsAPI

--- backend/src/test/java/com/devindev/congressoemchamas/externalapi/google/GoogleNewsAPITest.java
+++ backend/src/test/java/com/devindev/congressoemchamas/externalapi/google/GoogleNewsAPITest.java
@@ -1,4 +1,51 @@
 package com.devindev.congressoemchamas.externalapi.google;
 
+import com.devindev.congressoemchamas.TestUtils;
+import com.devindev.congressoemchamas.data.news.News;
+import com.devindev.congressoemchamas.externalapi.RequestsSender;
+import com.devindev.congressoemchamas.externalapi.google.functions.GetNewsByKeyword;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class GoogleNewsAPITest {
+
+    @InjectMocks
+    private GoogleNewsAPI googleNewsAPI;
+
+    @Mock
+    private GoogleNewsConfig googleNewsConfig;
+
+    @Mock
+    private RequestsSender requestsSender;
+
+    @Captor
+    private ArgumentCaptor<URI> uriCaptor;
+
+    @Test
+    public void requestNews_validKeyword_buildsRequestSuccessfully() throws IOException {
+        // given
+        String keyword = "Eduardo Bolsonaro";
+        News news1 = TestUtils.generateRandomNews();
+        News news2 = TestUtils.generateRandomNews();
+        // when
+        when(requestsSender.sendRequest(uriCaptor.capture(), any(GetNewsByKeyword.class))).thenReturn(Arrays.asList(news1, news2));
+        // then
+        List<News> actualOutput = googleNewsAPI.requestNews(keyword);
+        assertThat(actualOutput).containsExactly(news1, news2);
+        assertThat(uriCaptor.getValue().toString()).endsWith("https:?apiKey&language=pt&q=Deputado+Eduardo+Bolsonaro&sortBy=publishedAt");
+    }
 }
