f11d925f85ed81cd023abea65194588d70e990da
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-08-08 10:39:02 -0300

    Fixed bug with google configuration

--- src/main/java/com/devindev/congressoemchamas/externalapi/google/GoogleNewsConfig.java
+++ src/main/java/com/devindev/congressoemchamas/externalapi/google/GoogleNewsConfig.java
@@ -3,8 +3,10 @@ package com.devindev.congressoemchamas.externalapi.google;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:google.properties")
 public class GoogleNewsConfig {
 
     @Value("${google-news.api.key}")
