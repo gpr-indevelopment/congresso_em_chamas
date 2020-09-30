125bba61e96a8c9113088ae58eaff50ec96deba9
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-09-29 22:11:18 -0300

    Moved job launcher to test class

---
+++ backend/src/test/java/com/devindev/congressoemchamas/batch/JobLauncherTest.java
@@ -0,0 +1,27 @@
+package com.devindev.congressoemchamas.batch;
+
+import com.devindev.congressoemchamas.batch.reader.CamaraReader;
+import com.devindev.congressoemchamas.batch.writer.CamaraWriter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {CamaraWriter.class, CamaraReader.class, DataUpdaterStepsManager.class, DataUpdaterJobManager.class})
+public class JobLauncherTest {
+
+    @Autowired
+    private JobLauncher launcher;
+
+    @Autowired
+    private DataUpdaterJobManager dataUpdaterJobManager;
+
+    @Test
+    public void testBatch() throws Exception {
+        launcher.run(dataUpdaterJobManager.updatePoliticianData(), new JobParameters());
+    }
+}
