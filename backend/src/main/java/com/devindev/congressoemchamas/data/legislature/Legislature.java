4ac8b4960c07bc003f56c16563c0ecc8db1c67ed
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-10-07 22:25:36 -0300

    Added getValidityYears on Legislature

--- backend/src/main/java/com/devindev/congressoemchamas/data/legislature/Legislature.java
+++ backend/src/main/java/com/devindev/congressoemchamas/data/legislature/Legislature.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -18,4 +20,14 @@ public class Legislature implements Serializable {
     private YearMonth startDate;
 
     private YearMonth endDate;
+
+    public List<Integer> getValidityYears() {
+        List<Integer> years = new ArrayList<>();
+        Integer currentYear = startDate.getYear();
+        while(currentYear < endDate.getYear()){
+            years.add(currentYear);
+            currentYear++;
+        }
+        return years;
+    }
 }
