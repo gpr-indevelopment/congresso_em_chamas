7619036d3cffb1f2325a71e65407c0aeb2088e9a
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-12-05 17:10:54 -0300

    Added cache eviction for external api

--- backend/core/src/main/java/com/gprindevelopment/cec/externalapi/camara/CamaraConfig.java
+++ backend/core/src/main/java/com/gprindevelopment/cec/externalapi/camara/CamaraConfig.java
@@ -2,7 +2,9 @@ package com.gprindevelopment.cec.externalapi.camara;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CamaraConfig {}
