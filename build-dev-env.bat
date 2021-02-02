01486de9900b92bcc8aeadbecc1bd869f192b0ab
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-02-01 23:06:26 -0300

    Removed unused dev helpers

--- build-dev-env.bat
+++
@@ -1,6 +0,0 @@
-start "" code frontend
-start "" idea64 backend/pom.xml
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-timeout 60
-docker-compose -f docker-compose-dev.yml up -d
-exit
