26cbf4faf83142d2856c25173e27b56f260fa337
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-02-02 19:14:38 -0300

    Updated scheduling to main class

--- backend/core/src/main/java/com/gprindevelopment/cec/core/batch/BatchConfig.java
+++ backend/core/src/main/java/com/gprindevelopment/cec/core/batch/BatchConfig.java
@@ -3,11 +3,9 @@ package com.gprindevelopment.cec.core.batch;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @PropertySource("classpath:batch.properties")
-@EnableScheduling
 public class BatchConfig {
 
     @Value("${batch.politician.expiration-time-days}")
--- backend/web/src/main/java/com/gprindevelopment/cec/web/CongressoEmChamasApplication.java
+++ backend/web/src/main/java/com/gprindevelopment/cec/web/CongressoEmChamasApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Import(value = {CoreConfig.class})
 @EnableConfigurationProperties(CoreConfig.class)
+@EnableScheduling
 public class CongressoEmChamasApplication {
 
 	public static void main(String[] args) {
