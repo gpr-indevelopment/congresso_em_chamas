1ec6174045327b1af51dc40b909c7173ba20b8e2
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-02-10 22:56:23 -0300

    Cleaning cache on daily update

--- backend/core/src/main/java/com/gprindevelopment/cec/core/politician/DailyAccessedPoliticianDataUpdateScheduler.java
+++ backend/core/src/main/java/com/gprindevelopment/cec/core/politician/DailyAccessedPoliticianDataUpdateScheduler.java
@@ -1,6 +1,7 @@
 package com.gprindevelopment.cec.core.politician;
 
 import com.gprindevelopment.cec.core.batch.PoliticianDataUpdater;
+import com.gprindevelopment.cec.core.externalapi.ExternalAPICacheManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,19 +15,24 @@ public class DailyAccessedPoliticianDataUpdateScheduler {
 
     private final PoliticianDataUpdater politicianDataUpdater;
 
+    private final ExternalAPICacheManager externalAPICacheManager;
+
     private LinkedHashSet<Long> queueIds = new LinkedHashSet<>();
 
     public boolean addToQueue(Long politicianId) {
         return queueIds.add(politicianId);
     }
 
-    @Scheduled(cron = "0 0 1 * * MON-SAT")
+    @Scheduled(cron = "0 0 4 * * MON-SAT")
     public void executeJobs() throws Exception {
-        Iterator<Long> iterator = queueIds.iterator();
-        while(iterator.hasNext()) {
-            Long current = iterator.next();
-            politicianDataUpdater.batchUpdate(current);
-            iterator.remove();
+        if(!queueIds.isEmpty()) {
+            externalAPICacheManager.cleanCache();
+            Iterator<Long> iterator = queueIds.iterator();
+            while(iterator.hasNext()) {
+                Long current = iterator.next();
+                politicianDataUpdater.batchUpdate(current);
+                iterator.remove();
+            }
         }
     }
 }
