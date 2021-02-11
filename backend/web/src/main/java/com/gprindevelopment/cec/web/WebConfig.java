d409bb7093a7dbc8e57c79dd44ca7585c5ae112d
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-02-10 21:48:39 -0300

    Fixed view controllers

--- backend/web/src/main/java/com/gprindevelopment/cec/web/WebConfig.java
+++ backend/web/src/main/java/com/gprindevelopment/cec/web/WebConfig.java
@@ -9,9 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/expense").setViewName("index.html");
-        registry.addViewController("/news").setViewName("index.html");
-        registry.addViewController("/propositions").setViewName("index.html");
-        registry.addViewController("/search").setViewName("index.html");
+        registry.addViewController("/{spring:\\w+}").setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
     }
 }
