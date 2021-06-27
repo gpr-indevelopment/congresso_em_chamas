6ef4186df30cf13eeb6bfb802d307bc962394a66
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-06-26 23:59:59 -0300

    Revert "HTTP deploy"
    This reverts commit db6b9c0e3ce17380c652c9099fe162be9218f470.

--- backend/web/src/main/java/com/gprindevelopment/cec/web/HttpsConfiguration.java
+++ backend/web/src/main/java/com/gprindevelopment/cec/web/HttpsConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
+@Profile("!dev")
 public class HttpsConfiguration {
 
     @Value("${server.port.http}")
