db6b9c0e3ce17380c652c9099fe162be9218f470
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2021-06-26 23:01:26 -0300

    HTTP deploy

--- backend/web/src/main/java/com/gprindevelopment/cec/web/HttpsConfiguration.java
+++ backend/web/src/main/java/com/gprindevelopment/cec/web/HttpsConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!dev")
+@Profile("test")
 public class HttpsConfiguration {
 
     @Value("${server.port.http}")
