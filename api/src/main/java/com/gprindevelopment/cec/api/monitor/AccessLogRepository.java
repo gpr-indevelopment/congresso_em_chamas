919799a6f113cf54a28424415a2f9ec05f61ab16
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2022-07-12 17:00:03 -0300

    Removida funcionalidade de logs de acesso

--- api/src/main/java/com/gprindevelopment/cec/api/monitor/AccessLogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gprindevelopment.cec.api.monitor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
-}
